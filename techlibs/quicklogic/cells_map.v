d7389bb6b2b5d7a577b60eae0ebda2306bf90c05
Grzegorz Latosinski <glatosinski@antmicro.com>
2019-12-18 18:05:46 +0100

    quicklogic: Added address inversion for QuickLogic LUTs
    Signed-off-by: Grzegorz Latosinski <glatosinski@antmicro.com>

--- techlibs/quicklogic/cells_map.v
+++ techlibs/quicklogic/cells_map.v
@@ -5,22 +5,39 @@ module \$lut (A, Y);
     input [WIDTH-1:0] A;
     output Y;
 
+    function [WIDTH ** 2 - 1:0] INIT_address_inverse;
+        input [WIDTH ** 2 - 1: 0] arr;
+        integer i;
+        integer n;
+        reg [WIDTH - 1:0] tmp;
+        reg [WIDTH - 1:0] tmp2;
+        tmp = 0;
+        tmp2 = 0;
+        for (i = 0; i < WIDTH ** 2; i++) begin
+            INIT_address_inverse[tmp2] = arr[tmp];
+            tmp = tmp + 1;
+            for (n = 0; n < WIDTH; n++) begin
+                tmp2[WIDTH - 1 - n] = tmp[n];
+            end
+        end
+    endfunction
+
     generate
         if (WIDTH == 1)
         begin
-            LUT1 #(.INIT(LUT)) _TECHMAP_REPLACE_ (.O(Y), .I0(A[0]));
+            LUT1 #(.INIT(INIT_address_inverse(LUT))) _TECHMAP_REPLACE_ (.O(Y), .I0(A[0]));
         end
         else if (WIDTH == 2)
         begin
-            LUT2 #(.INIT(LUT)) _TECHMAP_REPLACE_ (.O(Y), .I0(A[0]), .I1(A[1]));
+            LUT2 #(.INIT(INIT_address_inverse(LUT))) _TECHMAP_REPLACE_ (.O(Y), .I0(A[0]), .I1(A[1]));
         end
         else if (WIDTH == 3)
         begin
-            LUT3 #(.INIT(LUT)) _TECHMAP_REPLACE_ (.O(Y), .I0(A[0]), .I1(A[1]), .I2(A[2]));
+            LUT3 #(.INIT(INIT_address_inverse(LUT))) _TECHMAP_REPLACE_ (.O(Y), .I0(A[0]), .I1(A[1]), .I2(A[2]));
         end
         else if (WIDTH == 4)
         begin
-            LUT4 #(.INIT(LUT)) _TECHMAP_REPLACE_ (.O(Y), .I0(A[0]), .I1(A[1]), .I2(A[2]), .I3(A[3]));
+            LUT4 #(.INIT(INIT_address_inverse(LUT))) _TECHMAP_REPLACE_ (.O(Y), .I0(A[0]), .I1(A[1]), .I2(A[2]), .I3(A[3]));
         end
         else
         begin
