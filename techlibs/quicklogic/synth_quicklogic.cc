c9431dc67e041671f6f3ad2f9159e05e2d58f999
Grzegorz Latosinski <glatosinski@antmicro.com>
2020-01-14 12:56:09 +0100

    quicklogic: Fixed the output name for ckpad.
    Signed-off-by: Grzegorz Latosinski <glatosinski@antmicro.com>

--- techlibs/quicklogic/synth_quicklogic.cc
+++ techlibs/quicklogic/synth_quicklogic.cc
@@ -126,7 +126,7 @@ struct SynthQuickLogicPass : public ScriptPass
             run("techmap -map +/quicklogic/cells_map.v");
             run("select -set clock_inputs */t:dff* %x:+[CLK] */t:dff* %d");
             run("select -set invclock_inputs */t:dff* %x:+[CLK] */t:dff* %d %n");
-            run("iopadmap -bits -inpad ckpad CLK:P @clock_inputs");
+            run("iopadmap -bits -inpad ckpad Q:P @clock_inputs");
             run("iopadmap -bits -outpad outpad A:P -inpad inpad Q:P @invclock_inputs");
             run("splitnets -ports");
             run("techmap -map +/quicklogic/cells_map.v");
