1792133e58146405568a6a5f5f28a70c29cb7854
Grzegorz Latosinski <glatosinski@antmicro.com>
2020-01-29 13:07:33 +0100

    quicklogic: Added mapping of VCC and GND in design to logic_1 and logic_0
    Signed-off-by: Grzegorz Latosinski <glatosinski@antmicro.com>

--- techlibs/quicklogic/synth_quicklogic.cc
+++ techlibs/quicklogic/synth_quicklogic.cc
@@ -128,7 +128,8 @@ struct SynthQuickLogicPass : public ScriptPass
             run("select -set invclock_inputs */t:dff* %x:+[CLK,CLR,PRE] */t:dff* %d %n");
             run("iopadmap -bits -inpad ckpad Q:P @clock_inputs");
             run("iopadmap -bits -outpad outpad A:P -inpad inpad Q:P @invclock_inputs");
-            run("splitnets -ports");
+            run("splitnets -ports -format ()");
+            run("hilomap -hicell logic_1 a -locell logic_0 a -singleton");
             run("techmap -map +/quicklogic/cells_map.v");
         }
 
