2335a7b46db2c5383e7a1031b0e2c2ffde3d2239
Maciej Kurc <mkurc@antmicro.com>
2020-02-18 14:30:57 +0100

    quicklogic: Corrected simulation models of LUTs for Quicklogic.
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

--- techlibs/quicklogic/cells_sim.v
+++ techlibs/quicklogic/cells_sim.v
@@ -7,36 +7,26 @@ endmodule
 //               TZ        TSL TAB
 module LUT2(output O, input I0, I1);
     parameter [3:0] INIT = 0;
-    wire [1:0] s1 = I0 ? INIT[3:2] : INIT[1:0];
-    assign O = I1 ? s1[1] : s1[0];
+    assign O = INIT[{I1, I0}];
 endmodule
 
-// O: TZ
+// O:  TZ
 // I0: TA1 TA2 TB1 TB2
 // I1: TSL
 // I2: TAB
 module LUT3(output O, input I0, I1, I2);
     parameter [7:0] INIT = 0;
-    wire [3:0] s2 = I0 ? INIT[7:4] : INIT[3:0];
-    wire [1:0] s1 = I1 ? s2[3:2] : s2[1:0];
-    assign O = I2 ? s1[1] : s1[0];
-    // TODO: This is not a valid in-sight implementation for QL - to be
-    // discussed with the client
+    assign O = INIT[{I2, I1, I0}];
 endmodule
 
-// O: CZ
+// O:  CZ
 // I0: TA1 TA2 TB1 TB2 BA1 BA2 BB1 BB2
 // I1: TSL BSL
 // I2: TAB BAB
 // I3: TBS
 module LUT4(output O, input I0, I1, I2, I3);
     parameter [15:0] INIT = 0;
-    wire [7:0] s3 = I0 ? INIT[15:8] : INIT[7:0];
-    wire [3:0] s2 = I1 ? s3[7:4] : s3[3:0];
-    wire [1:0] s1 = I2 ? s2[3:2] : s2[1:0];
-    assign O = I3 ? s1[1] : s1[0];
-    // TODO: This is not a valid in-sight implementation for QL - to be
-    // discussed with the client
+    assign O = INIT[{I3, I2, I1, I0}];
 endmodule
 
 //               FZ       FS
