ecd718222ea6894c44dcdb3fe37cc296317244da
Maciej Kurc <mkurc@antmicro.com>
2020-04-21 12:42:44 +0200

    Added tests for synth_quicklogic.
    Signed-off-by: Maciej Kurc <mkurc@antmicro.com>

---
+++ tests/arch/quicklogic/v/dffs.v
@@ -0,0 +1,34 @@
+module my_dff ( input d, clk, output reg q );
+    initial q <= 1'b0;
+    always @( posedge clk )
+        q <= d;
+endmodule
+
+module my_dffc ( input d, clk, clr, output reg q );
+    initial q <= 1'b0;
+    always @( posedge clk or posedge clr )
+        if ( clr )
+            q <= 1'b0;
+        else
+            q <= d;
+endmodule
+
+module my_dffp ( input d, clk, pre, output reg q );
+    initial q <= 1'b0;
+    always @( posedge clk or posedge pre )
+        if ( pre )
+            q <= 1'b1;
+        else
+            q <= d;
+endmodule
+
+module my_dffpc ( input d, clk, clr, pre, output reg q );
+    initial q <= 1'b0;
+    always @( posedge clk or posedge clr or posedge pre )
+        if ( clr )
+            q <= 1'b0;
+        else if ( pre )
+            q <= 1'b1;
+        else
+            q <= d;
+endmodule
