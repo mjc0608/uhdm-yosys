513e4d5dda371ebc4dff8fd652b6c720ab2242be
Karol Gugala <kgugala@antmicro.com>
2020-06-29 11:07:46 +0200

    QL: cells_map: map Valid_mult for mult16x16
    Signed-off-by: Karol Gugala <kgugala@antmicro.com>

--- techlibs/quicklogic/cells_map.v
+++ techlibs/quicklogic/cells_map.v
@@ -285,10 +285,12 @@ module qlal4s3_mult_16x16_cell (
     wire [63:0] Cmult_w;
     wire [31:0] Amult_w;
     wire [31:0] Bmult_w;
+    wire [1:0] Valid_mult;
 
     assign Amult_w = {{16{Amult[15]}}, Amult};
     assign Bmult_w = {{16{Bmult[15]}}, Bmult};
     assign Cmult = Cmult_w[31:0];
+    assign Valid_mult_w = {1'b0, Valid_mult[0]}
 
-    qlal4s3_mult_32x32_cell I1 ( .Amult(Amult_w), .Bmult(Bmult_w), .Cmult(Cmult_w));
+    qlal4s3_mult_32x32_cell I1 ( .Amult(Amult_w), .Bmult(Bmult_w), .Cmult(Cmult_w), .Valid_mult(Valid_mult_w));
 endmodule
