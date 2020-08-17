c1c7b21a08db0bebc106f6b77ea9ffd65d9af76c
Karol Gugala <kgugala@antmicro.com>
2020-08-17 09:07:06 +0200

    QL: refactor cells sim and synth script
    Signed-off-by: Karol Gugala <kgugala@antmicro.com>

--- techlibs/quicklogic/synth_quicklogic.cc
+++ techlibs/quicklogic/synth_quicklogic.cc
@@ -6,9 +6,9 @@
 USING_YOSYS_NAMESPACE
 PRIVATE_NAMESPACE_BEGIN
 
-struct SynthQuickLogicPass : public ScriptPass
-{
-    SynthQuickLogicPass() : ScriptPass("synth_quicklogic", "Synthesis for QuickLogic FPGAs") { }
+struct SynthQuickLogicPass : public ScriptPass {
+
+    SynthQuickLogicPass() : ScriptPass("synth_quicklogic", "Synthesis for QuickLogic FPGAs") {}
 
     void help() override
     {
@@ -19,6 +19,12 @@ struct SynthQuickLogicPass : public ScriptPass
         log("    -top <module>\n");
         log("         use the specified module as top module\n");
         log("\n");
+        log("    -family <family>\n");
+        log("        run synthesis for the specified QuickLogic architecture\n");
+        log("        generate the synthesis netlist for the specified family.\n");
+        log("        supported values:\n");
+        log("        - pp3: PolarPro 3 \n");
+        log("\n");
         log("    -edif <file>\n");
         log("        write the design to the specified edif file. writing of an output file\n");
         log("        is omitted if this parameter is not specified.\n");
@@ -27,25 +33,27 @@ struct SynthQuickLogicPass : public ScriptPass
         log("        write the design to the specified BLIF file. writing of an output file\n");
         log("        is omitted if this parameter is not specified.\n");
         log("\n");
+        log("The following commands are executed by this synthesis command:\n");
         help_script();
         log("\n");
     }
 
-    std::string top_opt = "";
-    std::string edif_file = "";
-    std::string blif_file = "";
-    std::string currmodule = "";
+    string top_opt, edif_file, blif_file, family, currmodule;
+    bool inferAdder;
 
     void clear_flags() override
     {
         top_opt = "-auto-top";
-        edif_file.clear();
-        blif_file.clear();
+        edif_file = "";
+        blif_file = "";
+        currmodule = "";
+        family = "pp3";
+        inferAdder = false;
     }
 
     void execute(std::vector<std::string> args, RTLIL::Design *design) override
     {
-        std::string run_from, run_to;
+        string run_from, run_to;
         clear_flags();
 
         size_t argidx;
@@ -57,6 +65,11 @@ struct SynthQuickLogicPass : public ScriptPass
             }
             if (args[argidx] == "-edif" && argidx+1 < args.size()) {
                 edif_file = args[++argidx];
+                continue;
+            }
+
+            if (args[argidx] == "-family" && argidx+1 < args.size()) {
+                family = args[++argidx];
                 continue;
             }
             if (args[argidx] == "-blif" && argidx+1 < args.size()) {
@@ -81,40 +94,104 @@ struct SynthQuickLogicPass : public ScriptPass
     void script() override
     {
         if (check_label("begin")) {
-            run("read_verilog -lib +/quicklogic/cells_sim.v");
-            run(stringf("hierarchy -check %s", top_opt.c_str()));
+            std::string readVelArgs = " +/quicklogic/" + family + "_cells_sim.v";
+            run("read_verilog -lib -specify +/quicklogic/cells_sim.v" + readVelArgs);
+            run(stringf("hierarchy -check %s", help_mode ? "-top <top>" : top_opt.c_str()));
         }
 
         if (check_label("prepare")) {
             run("proc");
             run("flatten");
-            run("wreduce -keepdc");
-            run("muxpack");
+            run("tribuf -logic");
+            run("opt_expr");
+            run("opt_clean");
+            run("deminout");
+            run("opt");
         }
 
         if (check_label("coarse")) {
-            run("tribuf -logic");
-            run("deminout");
-            run("opt");
+            run("opt_expr");
             run("opt_clean");
-            run("peepopt");
-            run("techmap");
-            run("muxcover -mux8 -mux4");
-            run("abc -luts 1,2,2,4");
-            run("opt");
             run("check");
+            run("opt");
+            run("wreduce -keepdc");
+            run("peepopt");
+            run("pmuxtree");
+            run("opt_clean");
+            run("share");
+            run("techmap -map +/cmp2lut.v -D LUT_WIDTH=4");
+            run("opt_expr");
+            run("opt_clean");
+
+            run("alumacc");
+            run("opt");
+            run("fsm");
+            run("opt -fast");
+            run("memory -nomap");
+            run("opt_clean");
         }
 
-        if (check_label("map")) {
-            run("techmap -map +/quicklogic/cells_map.v");
+        if (check_label("map_ffram")) {
+            run("opt -fast -mux_undef -undriven -fine");
+            run("memory_map -iattr -attr !ram_block -attr !rom_block -attr logic_block "
+                    "-attr syn_ramstyle=auto -attr syn_ramstyle=registers "
+                    "-attr syn_romstyle=auto -attr syn_romstyle=logic");
+            run("opt -undriven -fine");
+        }
+
+        if (check_label("map_gates")) {
+            run("techmap");
+            run("opt -fast");
+            run("muxcover -mux8 -mux4");
+            run("opt -fast");
+            run("opt_expr");
+            run("opt_merge");
+            run("opt_rmdff");
             run("opt_clean");
-            run("check");
+            run("opt");
+        }
+
+        if (check_label("map_ffs")) {
+            run("dff2dffe");
+
+            std::string techMapArgs = " -map +/quicklogic/" + family + "_ffs_map.v";
+            run("techmap " + techMapArgs);
+            run("opt_expr -mux_undef");
+            run("simplemap");
+            run("opt_expr");
+            run("opt_merge");
+            run("opt_rmdff");
+            run("opt_clean");
+            run("opt");
+        }
+
+        if (check_label("map_luts")) {
+            std::string techMapArgs = " -map +/quicklogic/" + family + "_latches_map.v";
+            run("techmap " + techMapArgs);
+            run("abc -luts 1,2,2");
+            techMapArgs = " -map +/quicklogic/" + family + "_ffs_map.v";
+            run("techmap " + techMapArgs);
+            run("clean");
+            run("opt_lut -dlogic QL_CARRY:I0=2:I1=1:CI=0");
+        }
+
+        if (check_label("map_cells")) {
+            std::string techMapArgs = " -map +/quicklogic/" + family + "_cells_map.v";
+            techMapArgs += " -map +/quicklogic/" + family + "_lut_map.v";
+            run("techmap" + techMapArgs);
+            run("clean");
+        }
+
+        if (check_label("check")) {
             run("autoname");
+            run("hierarchy -check");
+            run("stat");
+            run("check -noinit");
         }
 
         if (check_label("iomap")) {
-            run("clkbufmap -inpad ckpad Q:P");
-            run("iopadmap -bits -outpad outpad A:P -inpad inpad Q:P -tinoutpad bipad EN:Q:A:P -toutpad bipad EN:A:P A:top");
+            run("clkbufmap -buf $_BUF_ Y:A -inpad ckpad Q:P");
+            run("iopadmap -bits -outpad outpad A:P -inpad inpad Q:P -tinoutpad bipad EN:Q:A:P A:top");
         }
 
         if (check_label("finalize")) {
@@ -126,16 +203,17 @@ struct SynthQuickLogicPass : public ScriptPass
         }
 
         if (check_label("edif")) {
-            if (!edif_file.empty() || help_mode) {
+            if (!edif_file.empty() || help_mode)
                 run(stringf("write_edif -nogndvcc -attrprop -pvector par %s %s", this->currmodule.c_str(), edif_file.c_str()));
-            }
         }
 
         if (check_label("blif")) {
             if (!blif_file.empty() || help_mode)
                 run(stringf("write_blif %s %s", top_opt.c_str(), blif_file.c_str()));
         }
+
     }
-} SynthQuickLogicPass;
+
+} SynthQuicklogicPass;
 
 PRIVATE_NAMESPACE_END
